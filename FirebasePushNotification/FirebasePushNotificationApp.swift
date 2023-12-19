d82d599b71941f6ec667b02adb5bdcea60997701
CoderEslam <eslamghazy600@gmail.com>
2023-12-19 12:06:48 +0100

    Initial Commit

---
+++ FirebasePushNotification/FirebasePushNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebasePushNotificationApp.swift
+//  FirebasePushNotification
+//
+//  Created by Eslam Ghazy on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FirebasePushNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
